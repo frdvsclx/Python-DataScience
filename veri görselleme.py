0a9dba5914da90d39bc88a81c731b62ab39f085d
Firdevs Çolak <firdevscolak81@gmail.com>
2025-02-17 16:37:02 +0300

    .

--- veri görselleme.py
+++ veri görselleme.py
@@ -14,20 +14,23 @@ pd.set_option('display.width',500)
 pd.set_option('display.max_columns',None)
 
 df= sns.load_dataset("titanic")
+df.head(10)
+
 df["sex"].value_counts().plot(kind="bar") ##cinsiyet verisini sütun grafiğine gösterir
 plt.show() ##cıktı alınır
 
-plt.hist(df["age"])
+plt.hist(df["age"]) #yaslara göre histogram dagılımı
 plt.show()
 
-plt.bar
+plt.bar()
 
+#fare: yolculuk ücreti
 plt.boxplot(df["fare"]) #aykırı değerleri bulmakta iyi
 plt.show()
 
+#y=x^2 grafigi:
 x= np.array([1,2,3,4,5,6,7,8,9,10])
 y= np.array([2,4,8,16,32,64,128,256,512,1024])
-
-plt.plot(x,y)
-plt.plot(x,y,"o")
+plt.plot(x,y) #aynı indexleri birlestirir
+plt.plot(x,y,"o") #dot koyar
 plt.show()

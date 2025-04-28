1d31e9320d0063c0bf867d8bc712a7cdebd963f3
Firdevs Çolak <firdevscolak81@gmail.com>
2025-04-28 20:16:12 +0300

    .

--- analysis of correlation.py
+++ analysis of correlation.py
@@ -21,3 +21,34 @@ cor = df[num_cols].corr()
 sns.set(rc={'figure.figsize': (12,12)})
 sns.heatmap(cor,cmap="RdBu")
 plt.show()
+
+cor_matrix = df[num_cols].corr().abs()#positive
+
+#for repetitive data
+upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape),k=1).astype(np.bool_))
+
+#if cor is higher that .90 delete it
+drop_list =[col for col in upper_triangle_matrix if any(upper_triangle_matrix[col] > .90)]
+
+cor_matrix[drop_list]
+df.drop(drop_list, axis=1)
+
+def high_correlated_cols(dataframe,plot=False,corr_th=0.90):
+    numerical_col=[col for col in dataframe.columns if dataframe[col].dtype in [int,float]  ]
+    corr=dataframe[numerical_col].corr()
+    corr_matrix=corr.abs()
+    upper_triangle_matrix=cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(bool))
+    drop_list=[col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col]>0.90) ]
+
+    if plot:
+        sns.set(rc={'figure.figsize': (15,15)})
+        sns.heatmap(corr,cmap="RdBu")
+        plt.show()
+    return drop_list
+
+
+high_correlated_cols(df)
+drop_list=high_correlated_cols(df)
+df.drop(drop_list,axis=1)
+high_correlated_cols(df.drop(drop_list,axis=1), plot=True)
+

6883bfbdd0d44ceaab88b1c0a13e712a707f6e64
Firdevs Çolak <firdevscolak81@gmail.com>
2025-05-01 22:37:07 +0300

    .

--- veri görselleme.py
+++ veri görselleme.py
@@ -34,3 +34,16 @@ y= np.array([2,4,8,16,32,64,128,256,512,1024])
 plt.plot(x,y) #aynı indexleri birlestirir
 plt.plot(x,y,"o") #dot koyar
 plt.show()
+
+
+'''
+import matplotlib.pyplot as plt
+
+notlar = [68, 74, 82, 90, 78, 85, 92, 88, 76, 61, 79, 73, 89, 81, 72, 95, 70, 83, 77, 75]
+
+plt.hist(notlar, bins=10, edgecolor='r', alpha=0.7)
+plt.xlabel('Notlar')
+plt.ylabel('Frekans')
+plt.title('Sınav Notları Dağılımı')
+plt.show()
+'''
